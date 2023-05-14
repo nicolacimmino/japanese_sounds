cd63b0fdda54df3148d6a910a5eee7140f72389c
Nicola Cimmino <nicola.cimmino@gmail.com>
2023-05-14 18:47:00 +0200

    Cuckoo

--- pedestrian_crossing/cuckoo.rb
+++ pedestrian_crossing/cuckoo.rb
@@ -1,15 +1,18 @@
+# Cuckoo pedestrian crossing lights sound.
 
+f1 = hz_to_midi(1065)
+f2 = hz_to_midi(863)
 
 loop do
-  play hz_to_midi(1065), amp: 1, attack:0, sustain: 0.1, release: 0, pan: -1
+  play f1, amp: 1, attack:0, sustain: 0.1, release: 0, pan: -1
   sleep 0.480
-  play hz_to_midi(863), amp: 2, attack:0, sustain: 0.2, release: 0, pan: -1
+  play f2, amp: 2, attack:0, sustain: 0.2, release: 0, pan: -1
   sleep 1.200
-  play hz_to_midi(1065), amp: 1, attack:0, sustain: 0.1, release: 0, pan: 1
+  play f1, amp: 1, attack:0, sustain: 0.1, release: 0, pan: 1
   sleep 0.210
-  play hz_to_midi(1065), amp: 1, attack:0, sustain: 0.1, release: 0, pan: 1
+  play f1, amp: 1, attack:0, sustain: 0.1, release: 0, pan: 1
   sleep 0.170
-  play hz_to_midi(863), amp: 2, attack:0, sustain: 0.2, release: 0, pan: 1
+  play f2, amp: 2, attack:0, sustain: 0.2, release: 0, pan: 1
   sleep 1.2
 end
 
