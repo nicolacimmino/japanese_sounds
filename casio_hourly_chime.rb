6546c187cc3af44799809685fa7e22e10666876a
Nicola Cimmino <nicolacimmino@users.noreply.github.com>
2025-10-18 11:03:49 +0200

    Create casio_hourly_chime.rb

---
+++ casio_hourly_chime.rb
@@ -0,0 +1,14 @@
+# casio hourly chime
+use_synth :sine
+
+play f1, amp: 0.4, attack:0.005, sustain: 0.06, release: 0, pan: 0
+play f2, amp: 1, attack:0.005, sustain: 0.06, release: 0, pan: 0
+play f3, amp: 0.2, attack:0.005, sustain: 0.06, release: 0, pan: 0
+play f4, amp: 0.1, attack:0.005, sustain: 0.06, release: 0, pan: 0
+sleep 0.25
+
+play f1, amp: 0.4, attack:0.005, sustain: 0.06, release: 0, pan: 0
+play f2, amp: 1, attack:0.005, sustain: 0.06, release: 0, pan: 0
+play f3, amp: 0.2, attack:0.005, sustain: 0.06, release: 0, pan: 0
+play f4, amp: 0.1, attack:0.005, sustain: 0.06, release: 0, pan: 0
+sleep 0.25
